7f2aa7f94a180788aacaec1eccaadce6a7f3ae2e
Muminah Omolara <muminahadelabu3@gmail.com>
2026-02-16 07:06:53 +0100

    fixed deployment issues

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,47 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import waitlistRoutes from './routes/waitlistRoutes';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import waitlistRoutes from "./routes/waitlistRoutes";
 
 dotenv.config();
 
 const app: Express = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());              
-app.use(express.json());     
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/waitlist', waitlistRoutes);
+let isConnected = false;
 
 const connectToDb = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI as string)
-        console.log('Connected to MongoDB');
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-    }
+  if (isConnected) return;
+  if (!process.env.MONGO_URI) throw new Error("MONGO_URI is missing");
+  await mongoose.connect(process.env.MONGO_URI);
+  isConnected = true;
+};
+
+//connect db before handling requests
+app.use(async (req, res, next) => {
+  try {
+    await connectToDb();
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use("/api/waitlist", waitlistRoutes);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Bafrikart Server is Running!");
+});
+
+export default app;
+
+if (!process.env.VERCEL) {
+  app.listen(PORT, async () => {
+    await connectToDb();
+    console.log(`Server running on ${PORT}`);
+  });
 }
-
-//  Test if server is working
-app.get('/', (req: Request, res: Response) => {
-  res.send('Bafrikart Server is Running!');
-});
-
-
-app.listen(PORT, () => {
-  connectToDb();
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
